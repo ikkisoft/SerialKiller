143e34307a7826b06f0127e1ebefe6c00e68d201
Luca Carettoni <luca.carettoni@ikkisoft.com>
2016-09-07 10:43:37 -0700

    Profiling and logging features

--- src/org/nibblesec/tools/SerialKiller.java
+++ src/org/nibblesec/tools/SerialKiller.java
@@ -20,6 +20,10 @@ import java.io.InputStream;
 import java.io.InvalidClassException;
 import java.io.ObjectInputStream;
 import java.io.ObjectStreamClass;
+import java.util.logging.FileHandler;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import org.apache.commons.configuration.ConfigurationException;
@@ -30,42 +34,61 @@ public class SerialKiller extends ObjectInputStream {
 
     private final XMLConfiguration config;
     private final FileChangedReloadingStrategy reloadStrategy;
+    private static final Logger LOGGER = Logger.getLogger(SerialKiller.class.getName());
+    private static Handler fileHandler;
+    private static String logFile;
+    private static boolean logEnabled;
     private static String[] blacklist;
     private static String[] whitelist;
+    private static boolean profiling;    
 
     /**
      * SerialKiller constructor, returns instance of ObjectInputStream
      *
      * @param inputStream The original InputStream, used by your service to receive serialized objects
      * @param configFile The location of the config file (absolute path)
-     * @throws IOException ConfigurationException
+     * @throws java.io.IOException File I/O exception
+     * @throws org.apache.commons.configuration.ConfigurationException Config exception
      */
     public SerialKiller(InputStream inputStream, String configFile) throws IOException, ConfigurationException {
         
         super(inputStream);
+        
         config = new XMLConfiguration(configFile);
         reloadStrategy = new FileChangedReloadingStrategy();
         //To avoid permanent disc access on successive property lookups 
-        reloadStrategy.setRefreshDelay(config.getLong("refresh"));
+        reloadStrategy.setRefreshDelay(config.getLong("refresh", 6000));
         config.setReloadingStrategy(reloadStrategy);
+        
         blacklist = config.getStringArray("blacklist.regexp");
         whitelist = config.getStringArray("whitelist.regexp");
+        profiling = config.getBoolean("mode.profiling", false);
+        logEnabled = config.getBoolean("logging.enabled", true);        
+   
+        if(logEnabled){
+            logFile = config.getString("logging.logfile", "/tmp/serialkiller.log");
+            fileHandler  = new FileHandler(logFile, true);
+            LOGGER.addHandler(fileHandler);
+            LOGGER.setLevel(Level.ALL);
+        }
     }
 
     @Override
     protected Class<?> resolveClass(ObjectStreamClass serialInput) throws IOException, ClassNotFoundException {
-
-        if (reloadStrategy.reloadingRequired()) {
-            blacklist = config.getStringArray("blacklist.regexp");
-            whitelist = config.getStringArray("whitelist.regexp");
-        }
 
         //Enforce SerialKiller's blacklist
         for (String blackRegExp : blacklist) {
             Pattern blackPattern = Pattern.compile(blackRegExp);
             Matcher blackMatcher = blackPattern.matcher(serialInput.getName());
             if (blackMatcher.find()) {
-                throw new InvalidClassException("[!] Blocked by SerialKiller's blacklist '" + blackRegExp + "'. Match found for '" + serialInput.getName() + "'");
+                if (profiling){
+                    //Reporting mode 
+                    LOGGER.log(Level.FINE, "Blacklist match: ''{0}''", serialInput.getName());
+                }else{
+                    //Blocking mode
+                    LOGGER.log(Level.SEVERE, "Blocked by blacklist ''{0}''. Match found for ''{1}''", new Object[]{blackRegExp, serialInput.getName()});
+                    throw new InvalidClassException("Class blocked by SK: '" + serialInput.getName() + "'");
+                }
             }
         }
 
@@ -76,10 +99,17 @@ public class SerialKiller extends ObjectInputStream {
             Matcher whiteMatcher = whitePattern.matcher(serialInput.getName());
             if (whiteMatcher.find()) {
                 safeClass = true;
+                
+                if (profiling){
+                    //Reporting mode 
+                    LOGGER.log(Level.FINE, "Whitelist match: ''{0}''", serialInput.getName());
+                }
             }
         }
-        if (!safeClass) {
-            throw new InvalidClassException("[!] Blocked by SerialKiller's whitelist. No match found for '" + serialInput.getName() + "'");
+        if (!safeClass && !profiling) {
+            //Blocking mode
+            LOGGER.log(Level.SEVERE, "Blocked by whitelist. No match found for ''{0}''", serialInput.getName());
+            throw new InvalidClassException("Class blocked by SK: '" + serialInput.getName() + "'");
         }
 
         return super.resolveClass(serialInput);
